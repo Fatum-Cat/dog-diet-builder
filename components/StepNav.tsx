8b8d212d8f40871a5af2eb7bd83b156518d43a73
Fatum-Cat <petsecret.info@gmail.com>
2025-08-23 12:28:49 +0200

    Update StepNav.tsx

--- components/StepNav.tsx
+++ components/StepNav.tsx
@@ -1,1 +1,16 @@
+'use client';
+
+export default function StepNav({ canNext, onNext, onBack, showBack=true }:{
+  canNext:boolean; onNext:()=>void; onBack?:()=>void; showBack?:boolean;
+}){
+  return (
+    <div className="mt-8 flex justify-between">
+      {showBack ? <button onClick={onBack} className="btn">Back</button> : <div/>}
+      <button disabled={!canNext} onClick={onNext}
+        className={`btn btn-primary ${!canNext ? 'opacity-50 cursor-not-allowed' : ''}`}>
+        NEXT
+      </button>
+    </div>
+  );
+}
 
